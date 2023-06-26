11074023ab1bfb01e3a700a89624ed775e6e784c
Quddus Rahman <qrahman115@gmail.com>
2023-06-26 10:53:50 +0100

    completed variables4

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -3,10 +3,8 @@
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x = 3;
+    let mut x = 3; // in rust you can't change the value of a variable unless you declare it as mutable
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
