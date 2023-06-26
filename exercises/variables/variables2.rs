b39a0c615fc58cc07124ba331587e68de8612d96
Quddus Rahman <qrahman115@gmail.com>
2023-06-26 10:46:11 +0100

    completed variables3

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -3,10 +3,8 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
+    let x: u64 = 3; // tried adding a type annotation before the value (u64 is 64 bit integer)
     if x == 10 {
         println!("x is ten!");
     } else {
